33d76d8a65b965e407697467e91db836ffb20259
Luke Edwards <luke.edwards05@gmail.com>
2017-03-07 15:23:40 -0800

    add plugin logic

---
+++ index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const normalize = path.normalize;
+const sep = path.sep;
+
+module.exports = function (fly) {
+	fly.plugin('flatten', {every: false}, function * (files, opts) {
+		if (!files.length) {
+			return this.emit('plugin_warning', {
+				plugin: 'fly-flatten',
+				warning: 'No source files to flatten!'
+			});
+		}
+
+		const levels = opts.levels || 0;
+
+		// find shortest `dir` path; glob originated here
+		const globBaseDir = files.map(o => o.dir).sort()[0];
+
+		for (let file of files) {
+			file.dir = trimmer(globBaseDir, file.dir, levels);
+		}
+	});
+};
+
+function trimmer(baseDir, fileDir, levels) {
+	if (levels === 0) {
+		return baseDir;
+	}
+
+	let dirs = fileDir.replace(baseDir, '');
+	const arr = dirs.split(sep);
+	const len = arr.length;
+
+	if (levels < len) {
+		dirs = arr.splice(len - levels).join(sep);
+	}
+
+	return normalize(baseDir + sep + dirs);
+}
